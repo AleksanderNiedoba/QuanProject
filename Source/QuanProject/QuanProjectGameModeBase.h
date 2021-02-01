d3c0e75217b9e2cab6438802a720a727f6d7c795
DESKTOP-7VCDJAG\aleks <aleksander.niedoba@gmail.com>
2021-02-01 19:57:18 +0100

    Initail commit. Clear UE 4.26 project files.

---
+++ Source/QuanProject/QuanProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuanProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUANPROJECT_API AQuanProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
