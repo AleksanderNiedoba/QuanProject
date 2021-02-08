6917b40897d7ec5b329f508407aa630797414e2c
DESKTOP-7VCDJAG\aleks <aleksander.niedoba@gmail.com>
2021-02-08 12:57:06 +0100

    Added events in interact for component for each interact action.

--- Plugins/OverworldBase/Source/OverworldBase/Public/InteractComponent.h
+++ Plugins/OverworldBase/Source/OverworldBase/Public/InteractComponent.h
@@ -7,6 +7,10 @@
 #include "InteractComponent.generated.h"
 
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FComponentHovered);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FComponentRelesedHovere);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FComponentClicked);
+
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class OVERWORLDBASE_API UInteractComponent : public UActorComponent
 {
@@ -16,19 +20,26 @@ public:
 	// Sets default values for this component's properties
 	UInteractComponent();
 
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
 
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	
-
 	virtual void OnHover();
 	virtual void ReleaseHover();
 	virtual void OnClick();
-		
+
+public:
+	//Component events 
+	UPROPERTY(BlueprintAssignable)
+	FComponentHovered OnHovered;
+	UPROPERTY(BlueprintAssignable)
+	FComponentRelesedHovere OnHoverReleased;
+	UPROPERTY(BlueprintAssignable)
+	FComponentClicked ComponentClicked;
 };
